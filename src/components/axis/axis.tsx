31411da3b94b2c8d47009ef28e17e573daeb1e21
michele landolfi <m.landolfi@apio.cc>
2025-05-09 12:26:19 +0200

    fix: added svg title for a11y and color prop fol axis labels

--- src/components/axis/axis.tsx
+++ src/components/axis/axis.tsx
@@ -12,280 +12,287 @@ import { isFunction } from "../../lib/utils";
 import { nanoid } from "nanoid";
 /* Context Imports */
 import {
-  useCharts,
-  useChartsDispatch,
-  useChartsTheme,
+	useCharts,
+	useChartsDispatch,
+	useChartsTheme,
 } from "../../contexts/chartContext";
 
 export type AxisProps = {
-  type: "xAxis" | "yAxis";
-  name?: string;
-  dataPoints?: string[];
-  labelSize?: number;
-  titleSize?: number;
-  showGrid?: boolean;
-  showLine?: boolean;
-  titleDx?: number;
-  titleDy?: number;
-  showName?: boolean;
+	type: "xAxis" | "yAxis";
+	name?: string;
+	dataPoints?: string[];
+	labelSize?: number;
+	labelColor?: string;
+	titleSize?: number;
+	showGrid?: boolean;
+	showLine?: boolean;
+	titleDx?: number;
+	titleDy?: number;
+	showName?: boolean;
 };
 
 const Axis = (props: AxisProps) => {
-  const {
-    type,
-    name,
-    dataPoints = [],
-    showGrid = false,
-    labelSize = undefined,
-    titleSize = undefined,
-    showName = false,
-    titleDx = 0,
-    titleDy = 0,
-    showLine = false,
-  } = props;
+	const {
+		type,
+		name,
+		dataPoints = [],
+		showGrid = false,
+		labelSize = undefined,
+		labelColor = undefined,
+		titleSize = undefined,
+		showName = false,
+		titleDx = 0,
+		titleDy = 0,
+		showLine = false,
+	} = props;
 
-  const ctx = useCharts();
+	const ctx = useCharts();
 
-  const dispatch = useChartsDispatch();
+	const dispatch = useChartsDispatch();
 
-  const theme = useChartsTheme();
+	const theme = useChartsTheme();
 
-  if (!ctx || !theme) return null;
+	if (!ctx || !theme) return null;
 
-  const { padding, yInterval } = theme;
+	const { padding, yInterval } = theme;
 
-  const {
-    chartXEnd,
-    chartXStart,
-    chartYEnd,
-    elements,
-    hoveredElement,
-    chartID,
-    globalConfig,
-  } = ctx;
+	const {
+		chartXEnd: _chartXEnd,
+		chartXStart: _chartXStart,
+		chartYEnd: _chartYEnd,
+		elements,
+		hoveredElement,
+		chartID,
+		globalConfig,
+	} = ctx;
 
-  const tooltipElement = document?.getElementById(`cts-tooltip-${chartID}`);
+	const chartXEnd = _chartXEnd as number;
+	const chartXStart = _chartXStart as number;
+	const chartYEnd = _chartYEnd as number;
 
-  const labelFontSize = labelSize ?? theme?.axis?.labelSize;
+	const tooltipElement = document?.getElementById(`cts-tooltip-${chartID}`);
 
-  // Creazione dell'asse X
-  if (type === "xAxis") {
-    const xAxis = generateXAxis(ctx);
+	const labelFontSize = labelSize ?? theme?.axis?.labelSize;
 
-    const serie = elements?.[0] || { data: [] };
+	const labelTextColor = labelColor ?? theme.axis?.labelColor;
 
-    const serieData = serie.data as TimeSerieEl[];
+	// Creazione dell'asse X
+	if (type === "xAxis") {
+		const xAxis = generateXAxis(ctx);
 
-    const xAxisInterval =
-      (chartXEnd! - chartXStart!) / (serieData?.length || 1);
+		const serie = elements?.[0] || { data: [] };
 
-    const selectionColor = globalConfig?.selectedColor as string;
-    const selectionValue = globalConfig?.selectedValue as string;
+		const serieData = serie.data as TimeSerieEl[];
 
-    const labels = dataPoints.map((label, labelIndex) => {
-      const xSpacing = globalConfig?.barWidth
-        ? (Number(globalConfig?.barWidth) + padding) / 2
-        : padding;
+		const xAxisInterval = (chartXEnd - chartXStart) / (serieData?.length || 1);
 
-      return {
-        value: label,
-        x: xAxisInterval * labelIndex + chartXStart! + xSpacing,
-        y: chartYEnd! + padding,
-      };
-    });
+		const selectionColor = globalConfig?.selectedColor as string;
+		const selectionValue = globalConfig?.selectedValue as string;
 
-    const xPoints = labels.map((label, labelIndex) => {
-      const hoverRectX = label.x - (xAxisInterval - padding) / 2;
-      const hoverRectWidth = xAxisInterval - padding;
+		const labels = dataPoints.map((label, labelIndex) => {
+			const xSpacing = globalConfig?.barWidth
+				? (Number(globalConfig?.barWidth) + padding) / 2
+				: padding;
 
-      const selectionFill =
-        label.value === selectionValue ? `${selectionColor}26` : undefined;
+			return {
+				value: label,
+				x: xAxisInterval * labelIndex + chartXStart + xSpacing,
+				y: chartYEnd + padding,
+			};
+		});
 
-      const height =
-        label.value === selectionValue ? (chartYEnd ?? 0) + 35 : chartYEnd;
+		const xPoints = labels.map((label, labelIndex) => {
+			const hoverRectX = label.x - (xAxisInterval - padding) / 2;
+			const hoverRectWidth = xAxisInterval - padding;
 
-      const labelFontWeight = label.value === selectionValue ? 700 : 400;
+			const selectionFill =
+				label.value === selectionValue ? `${selectionColor}26` : undefined;
 
-      const fill: string =
-        hoveredElement?.elementIndex === labelIndex &&
-        globalConfig?.barClickAction
-          ? "rgb(148,163,184,0.1)"
-          : selectionFill
-            ? selectionFill
-            : "transparent";
+			const height =
+				label.value === selectionValue ? (chartYEnd ?? 0) + 35 : chartYEnd;
 
-      return (
-        <Fragment key={`${label.value}-${nanoid()}`}>
-          <>
-            {showGrid ? (
-              <path
-                d={`M ${label.x} ${chartYEnd! + (padding * 1) / 3} V 0`}
-                strokeWidth={theme?.grid?.size}
-                strokeDasharray={theme?.grid?.dashed ? 5 : 0}
-                stroke={theme?.grid?.color}
-              />
-            ) : null}
-            {tooltipElement ? (
-              <rect
-                onClick={() => {
-                  if (
-                    globalConfig?.barClickAction &&
-                    isFunction(globalConfig.barClickAction)
-                  ) {
-                    const serieEl = serieData[labelIndex];
-                    globalConfig.barClickAction(serieEl);
-                  }
-                }}
-                onMouseEnter={() => {
-                  if (dispatch && labelIndex !== hoveredElement?.elementIndex) {
-                    dispatch({
-                      type: "SET_HOVER_ELEMENT",
-                      payload: {
-                        hoveredElement: {
-                          elementIndex: labelIndex,
-                          label: label.value,
-                        },
-                      },
-                    });
-                  }
-                }}
-                x={hoverRectX > 0 ? hoverRectX : 0}
-                y={0}
-                width={hoverRectWidth > 0 ? hoverRectWidth : 1}
-                height={height}
-                fill={fill}
-              />
-            ) : null}
-          </>
-          {dataPoints.length > 20 ? (
-            <>
-              <defs>
-                <path
-                  id={`xAxisLabel-${labelIndex}`}
-                  d={`M ${label.x - 40} ${label.y + 20} L ${label.x} ${label.y}`}
-                />
-              </defs>
-              {/* <use href={`#xAxisLabel-${labelIndex}`} fill="none" /> */}
-              <text
-                dx={titleDx}
-                dy={titleDy}
-                fontSize={theme?.axis?.labelSize}
-                fontWeight={labelFontWeight}
-                fill={theme?.axis?.labelColor}
-              >
-                <textPath
-                  textAnchor="start"
-                  x={label.x}
-                  y={label.y}
-                  href={`#xAxisLabel-${labelIndex}`}
-                >
-                  {label.value}
-                </textPath>
-              </text>
-            </>
-          ) : (
-            <text
-              textAnchor="middle"
-              x={label.x}
-              y={label.y}
-              fontSize={labelFontSize}
-              fontWeight={labelFontWeight}
-              fill={theme?.axis?.labelColor}
-            >
-              {label.value}
-            </text>
-          )}
-        </Fragment>
-      );
-    });
+			const labelFontWeight = label.value === selectionValue ? 700 : 400;
 
-    return (
-      <>
-        {showLine ? (
-          <path
-            d={xAxis?.path}
-            strokeWidth={theme?.axis?.size}
-            stroke={theme?.axis?.color}
-          />
-        ) : null}
-        {xPoints}
-        {showName ? (
-          <text
-            x={chartXEnd! / 2}
-            y={chartYEnd! + 45}
-            textAnchor="middle"
-            fontSize={titleSize ?? theme?.axis?.titleSize}
-            fill={theme?.axis?.titleColor}
-            fontWeight={600}
-          >
-            {name}
-          </text>
-        ) : null}
-      </>
-    );
-  }
+			const fill: string =
+				hoveredElement?.elementIndex === labelIndex &&
+				globalConfig?.barClickAction
+					? "rgb(148,163,184,0.1)"
+					: selectionFill
+						? selectionFill
+						: "transparent";
 
-  const serieElement = elements?.find((el) => el.name === name);
+			return (
+				<Fragment key={`${label.value}-${nanoid()}`}>
+					<>
+						{showGrid ? (
+							<path
+								d={`M ${label.x} ${chartYEnd + (padding * 1) / 3} V 0`}
+								strokeWidth={theme?.grid?.size}
+								strokeDasharray={theme?.grid?.dashed ? 5 : 0}
+								stroke={theme?.grid?.color}
+							/>
+						) : null}
+						{tooltipElement ? (
+							<rect
+								onClick={() => {
+									if (
+										globalConfig?.barClickAction &&
+										isFunction(globalConfig.barClickAction)
+									) {
+										const serieEl = serieData[labelIndex];
+										globalConfig.barClickAction(serieEl);
+									}
+								}}
+								onMouseEnter={() => {
+									if (dispatch && labelIndex !== hoveredElement?.elementIndex) {
+										dispatch({
+											type: "SET_HOVER_ELEMENT",
+											payload: {
+												hoveredElement: {
+													elementIndex: labelIndex,
+													label: label.value,
+												},
+											},
+										});
+									}
+								}}
+								x={hoverRectX > 0 ? hoverRectX : 0}
+								y={0}
+								width={hoverRectWidth > 0 ? hoverRectWidth : 1}
+								height={height}
+								fill={fill}
+							/>
+						) : null}
+					</>
+					{dataPoints.length > 20 ? (
+						<>
+							<defs>
+								<path
+									id={`xAxisLabel-${labelIndex}`}
+									d={`M ${label.x - 40} ${label.y + 20} L ${label.x} ${label.y}`}
+								/>
+							</defs>
+							{/* <use href={`#xAxisLabel-${labelIndex}`} fill="none" /> */}
+							<text
+								dx={titleDx}
+								dy={titleDy}
+								fontSize={theme?.axis?.labelSize}
+								fontWeight={labelFontWeight}
+								fill={labelTextColor}
+							>
+								<textPath
+									textAnchor="start"
+									x={label.x}
+									y={label.y}
+									href={`#xAxisLabel-${labelIndex}`}
+								>
+									{label.value}
+								</textPath>
+							</text>
+						</>
+					) : (
+						<text
+							textAnchor="middle"
+							x={label.x}
+							y={label.y}
+							fontSize={labelFontSize}
+							fontWeight={labelFontWeight}
+							fill={labelTextColor}
+						>
+							{label.value}
+						</text>
+					)}
+				</Fragment>
+			);
+		});
 
-  if (!serieElement) return null;
+		return (
+			<>
+				{showLine ? (
+					<path
+						d={xAxis?.path}
+						strokeWidth={theme?.axis?.size}
+						stroke={theme?.axis?.color}
+					/>
+				) : null}
+				{xPoints}
+				{showName ? (
+					<text
+						x={chartXEnd / 2}
+						y={chartYEnd + 45}
+						textAnchor="middle"
+						fontSize={titleSize ?? theme?.axis?.titleSize}
+						fill={theme?.axis?.titleColor}
+						fontWeight={600}
+					>
+						{name}
+					</text>
+				) : null}
+			</>
+		);
+	}
 
-  // Creazione dell'asse Y
-  const yAxis = generateYAxis(serieElement, { ...ctx, padding, yInterval });
+	const serieElement = elements?.find((el) => el.name === name);
 
-  if (!yAxis) return null;
+	if (!serieElement) return null;
 
-  return (
-    <Fragment>
-      {showName ? (
-        <>
-          <defs>
-            <path d={yAxis.nameLabelPath} id={`axis-${yAxis.nameLabelPath}`} />
-          </defs>
-          <text
-            dy={titleDy}
-            dx={titleDx}
-            fontSize={titleSize ?? theme?.axis?.titleSize}
-            fill={theme?.axis?.titleColor}
-            fontWeight={600}
-            textAnchor="middle"
-            dominantBaseline="middle"
-          >
-            <textPath startOffset="50%" href={`#axis-${yAxis.nameLabelPath}`}>
-              {yAxis.uom ? `${yAxis.name} (${yAxis.uom})` : `${yAxis.name}`}
-            </textPath>
-          </text>
-        </>
-      ) : null}
-      {yAxis.valueLabels.map((label, labelIndex) => (
-        <Fragment key={`${yAxis.name}-${label.value}-${nanoid()}`}>
-          <text
-            textAnchor={yAxis.isOpposite ? "start" : "end"}
-            fontSize={labelFontSize}
-            x={label.x}
-            y={label.y + (labelFontSize ?? 0) / 2}
-            fill={theme?.axis?.labelColor}
-          >
-            {label.value}
-          </text>
-          {showGrid && labelIndex > 0 ? (
-            <path
-              d={`M ${chartXStart! + padding / 4} ${label.y} H ${chartXEnd! - padding / 4}`}
-              strokeWidth={theme?.grid?.size}
-              strokeDasharray={theme?.grid?.dashed ? 5 : 0}
-              stroke={theme?.grid?.color}
-            />
-          ) : null}
-        </Fragment>
-      ))}
-      {showLine ? (
-        <path
-          d={yAxis.path}
-          strokeWidth={theme?.axis?.size}
-          stroke={theme?.axis?.color}
-        />
-      ) : null}
-    </Fragment>
-  );
+	// Creazione dell'asse Y
+	const yAxis = generateYAxis(serieElement, { ...ctx, padding, yInterval });
+
+	if (!yAxis) return null;
+
+	return (
+		<Fragment>
+			{showName ? (
+				<>
+					<defs>
+						<path d={yAxis.nameLabelPath} id={`axis-${yAxis.nameLabelPath}`} />
+					</defs>
+					<text
+						dy={titleDy}
+						dx={titleDx}
+						fontSize={titleSize ?? theme?.axis?.titleSize}
+						fill={theme?.axis?.titleColor}
+						fontWeight={600}
+						textAnchor="middle"
+						dominantBaseline="middle"
+					>
+						<textPath startOffset="50%" href={`#axis-${yAxis.nameLabelPath}`}>
+							{yAxis.uom ? `${yAxis.name} (${yAxis.uom})` : `${yAxis.name}`}
+						</textPath>
+					</text>
+				</>
+			) : null}
+			{yAxis.valueLabels.map((label, labelIndex) => (
+				<Fragment key={`${yAxis.name}-${label.value}-${nanoid()}`}>
+					<text
+						textAnchor={yAxis.isOpposite ? "start" : "end"}
+						fontSize={labelFontSize}
+						x={label.x}
+						y={label.y + (labelFontSize ?? 0) / 2}
+						fill={labelTextColor}
+					>
+						{label.value}
+					</text>
+					{showGrid && labelIndex > 0 ? (
+						<path
+							d={`M ${chartXStart + padding / 4} ${label.y} H ${chartXEnd - padding / 4}`}
+							strokeWidth={theme?.grid?.size}
+							strokeDasharray={theme?.grid?.dashed ? 5 : 0}
+							stroke={theme?.grid?.color}
+						/>
+					) : null}
+				</Fragment>
+			))}
+			{showLine ? (
+				<path
+					d={yAxis.path}
+					strokeWidth={theme?.axis?.size}
+					stroke={theme?.axis?.color}
+				/>
+			) : null}
+		</Fragment>
+	);
 };
 
 export default Axis;
--- src/components/svg/svg.tsx
+++ src/components/svg/svg.tsx
@@ -1,207 +1,208 @@
 /* Types Imports */
 import {
-  useCallback,
-  type MouseEvent,
-  type MouseEventHandler,
-  useEffect,
-  useRef,
-  RefObject,
-  ReactNode,
-  JSX,
+	type JSX,
+	type MouseEvent,
+	type MouseEventHandler,
+	type ReactNode,
+	type RefObject,
+	useCallback,
+	useEffect,
+	useRef,
 } from "react";
 
 /* Core Imports */
 import {
-  calculateTooltipPosition,
-  convertToSVGPoint,
-  getChartDimensions,
+	calculateTooltipPosition,
+	convertToSVGPoint,
+	getChartDimensions,
 } from "../../lib/core";
 
 /* Context Imports */
 import {
-  useCharts,
-  useChartsDispatch,
-  useChartsTheme,
+	useCharts,
+	useChartsDispatch,
+	useChartsTheme,
 } from "../../contexts/chartContext";
 import { isDefined } from "../../lib/utils";
 import { DEFAULT_LEGEND_HEIGHT } from "../legend/legend";
 
 type SVGProps = {
-  children: ReactNode;
-  containerRef: RefObject<HTMLDivElement | null>;
-  chartID: string | null;
-  leftAxisCount?: number;
-  rightAxisCount?: number;
+	children: ReactNode;
+	containerRef: RefObject<HTMLDivElement | null>;
+	chartID: string | null;
+	leftAxisCount?: number;
+	rightAxisCount?: number;
 };
 
 // Funzione che prende tutte le props di config degli elementi grafici e le setta nel context globale
 const computeConfigs = (children: JSX.Element[]) => {
-  const elementsWithConfig = children.filter(
-    (childEl) => childEl.props?.config,
-  );
+	const elementsWithConfig = children.filter(
+		(childEl) => childEl.props?.config,
+	);
 
-  if (elementsWithConfig.length <= 0) return {};
+	if (elementsWithConfig.length <= 0) return {};
 
-  const globalConfig = elementsWithConfig.reduce((acc, el) => {
-    const elConfig = el.props.config;
-    const newAcc = {
-      ...acc,
-      ...elConfig,
-    };
+	const globalConfig = elementsWithConfig.reduce((acc, el) => {
+		const elConfig = el.props.config;
+		const newAcc = {
+			...acc,
+			...elConfig,
+		};
 
-    return newAcc;
-  }, {});
+		return newAcc;
+	}, {});
 
-  return globalConfig;
+	return globalConfig;
 };
 
 // Funzione che calcola ricava l'altezza della legenda
 const getLegendHeight = (children: JSX.Element[]) => {
-  /* Verfico che esista un elemento Legend */
-  const legend = children.find(
-    (childEl) =>
-      childEl.props.legendType !== null &&
-      childEl.props.legendType !== undefined,
-  );
+	/* Verfico che esista un elemento Legend */
+	const legend = children.find(
+		(childEl) =>
+			childEl.props?.legendType !== null &&
+			childEl.props?.legendType !== undefined,
+	);
 
-  if (!legend) return 0;
+	if (!legend) return 0;
 
-  /* Se l'elemento Legend esiste prendo la sua altezza */
-  const legendHeight = legend.props?.height
-    ? legend?.props?.height
-    : DEFAULT_LEGEND_HEIGHT;
+	/* Se l'elemento Legend esiste prendo la sua altezza */
+	const legendHeight = legend.props?.height
+		? legend?.props?.height
+		: DEFAULT_LEGEND_HEIGHT;
 
-  return legendHeight;
+	return legendHeight;
 };
 
 const Svg = (props: SVGProps) => {
-  const { children, containerRef, leftAxisCount, rightAxisCount, chartID } =
-    props;
+	const { children, containerRef, leftAxisCount, rightAxisCount, chartID } =
+		props;
 
-  const rootRef = useRef<SVGSVGElement>(null);
+	const rootRef = useRef<SVGSVGElement>(null);
 
-  const ctx = useCharts();
-  const dispatch = useChartsDispatch();
-  const theme = useChartsTheme();
+	const ctx = useCharts();
+	const dispatch = useChartsDispatch();
+	const theme = useChartsTheme();
 
-  const { padding } = theme ?? {};
+	const { padding } = theme ?? {};
 
-  const normalizedChildren = Array.isArray(children) ? children : [children];
+	const normalizedChildren = Array.isArray(children) ? children : [children];
 
-  const legendHeight = getLegendHeight(normalizedChildren);
+	const legendHeight = getLegendHeight(normalizedChildren);
 
-  const globalConfig = useRef(computeConfigs(normalizedChildren));
+	const globalConfig = useRef(computeConfigs(normalizedChildren));
 
-  // Funzione che inizializza le dimensioni del grafico svg
-  const intializeChart = useCallback(() => {
-    if (
-      dispatch &&
-      rootRef.current &&
-      containerRef.current &&
-      isDefined(padding) &&
-      isDefined(leftAxisCount) &&
-      isDefined(rightAxisCount)
-    ) {
-      const { chartXStart, chartXEnd, chartYEnd } = getChartDimensions(
-        padding as number,
-        containerRef.current.clientWidth,
-        containerRef.current.clientHeight,
-        leftAxisCount as number,
-        rightAxisCount as number,
-        legendHeight,
-      );
-      dispatch({
-        type: "INITIALIZE",
-        payload: {
-          svgRef: rootRef.current,
-          width: containerRef.current.clientWidth,
-          chartXStart,
-          chartXEnd,
-          chartYEnd,
-          chartID,
-          globalConfig: globalConfig.current,
-        },
-      });
-    }
-  }, [
-    dispatch,
-    containerRef,
-    padding,
-    rightAxisCount,
-    leftAxisCount,
-    chartID,
-    legendHeight,
-  ]);
+	// Funzione che inizializza le dimensioni del grafico svg
+	const intializeChart = useCallback(() => {
+		if (
+			dispatch &&
+			rootRef.current &&
+			containerRef.current &&
+			isDefined(padding) &&
+			isDefined(leftAxisCount) &&
+			isDefined(rightAxisCount)
+		) {
+			const { chartXStart, chartXEnd, chartYEnd } = getChartDimensions(
+				padding as number,
+				containerRef.current.clientWidth,
+				containerRef.current.clientHeight,
+				leftAxisCount as number,
+				rightAxisCount as number,
+				legendHeight,
+			);
+			dispatch({
+				type: "INITIALIZE",
+				payload: {
+					svgRef: rootRef.current,
+					width: containerRef.current.clientWidth,
+					chartXStart,
+					chartXEnd,
+					chartYEnd,
+					chartID,
+					globalConfig: globalConfig.current,
+				},
+			});
+		}
+	}, [
+		dispatch,
+		containerRef,
+		padding,
+		rightAxisCount,
+		leftAxisCount,
+		chartID,
+		legendHeight,
+	]);
 
-  useEffect(() => {
-    intializeChart();
-    window.addEventListener("resize", () => intializeChart());
-    return () => window.removeEventListener("resize", () => intializeChart());
-  }, [intializeChart]);
+	useEffect(() => {
+		intializeChart();
+		window.addEventListener("resize", () => intializeChart());
+		return () => window.removeEventListener("resize", () => intializeChart());
+	}, [intializeChart]);
 
-  const { svgRef, chartXStart, chartXEnd, chartYEnd, width, height } =
-    ctx ?? {};
+	const { svgRef, chartXStart, chartXEnd, chartYEnd, width, height } =
+		ctx ?? {};
 
-  const handleMouseLeave = () => {
-    const tooltipElement = document.getElementById(`cts-tooltip-${chartID}`);
-    if (tooltipElement) {
-      tooltipElement.style.display = "none";
-    }
-  };
+	const handleMouseLeave = () => {
+		const tooltipElement = document.getElementById(`cts-tooltip-${chartID}`);
+		if (tooltipElement) {
+			tooltipElement.style.display = "none";
+		}
+	};
 
-  const handleMouseMove: MouseEventHandler<SVGSVGElement> = useCallback(
-    (event: MouseEvent) => {
-      const tooltipElement = document.getElementById(`cts-tooltip-${chartID}`);
-      if (tooltipElement?.style.display === "none") {
-        tooltipElement.style.display = "block";
-      }
-      const { clientX, clientY } = event;
-      if (
-        svgRef &&
-        tooltipElement &&
-        dispatch &&
-        chartXStart !== undefined &&
-        chartXEnd !== undefined &&
-        chartYEnd !== undefined
-      ) {
-        const svgPoint = convertToSVGPoint(svgRef, clientX, clientY) ?? {
-          x: 0,
-          y: 0,
-        };
+	const handleMouseMove: MouseEventHandler<SVGSVGElement> = useCallback(
+		(event: MouseEvent) => {
+			const tooltipElement = document.getElementById(`cts-tooltip-${chartID}`);
+			if (tooltipElement?.style.display === "none") {
+				tooltipElement.style.display = "block";
+			}
+			const { clientX, clientY } = event;
+			if (
+				svgRef &&
+				tooltipElement &&
+				dispatch &&
+				chartXStart !== undefined &&
+				chartXEnd !== undefined &&
+				chartYEnd !== undefined
+			) {
+				const svgPoint = convertToSVGPoint(svgRef, clientX, clientY) ?? {
+					x: 0,
+					y: 0,
+				};
 
-        const tooltipPosition = calculateTooltipPosition(
-          tooltipElement,
-          svgPoint,
-          chartXStart,
-          chartXEnd,
-          chartYEnd,
-        );
+				const tooltipPosition = calculateTooltipPosition(
+					tooltipElement,
+					svgPoint,
+					chartXStart,
+					chartXEnd,
+					chartYEnd,
+				);
 
-        dispatch({
-          type: "SET_TOOLTIP_POSITION",
-          payload: { mousePosition: svgPoint, tooltipPosition },
-        });
-      }
-    },
-    [svgRef, dispatch, chartXStart, chartXEnd, chartYEnd, chartID],
-  );
+				dispatch({
+					type: "SET_TOOLTIP_POSITION",
+					payload: { mousePosition: svgPoint, tooltipPosition },
+				});
+			}
+		},
+		[svgRef, dispatch, chartXStart, chartXEnd, chartYEnd, chartID],
+	);
 
-  if (!height) return null;
+	if (!height) return null;
 
-  const viewBox = `0 0 ${width} ${height + legendHeight}`;
+	const viewBox = `0 0 ${width} ${height + legendHeight}`;
 
-  return (
-    <svg
-      ref={rootRef}
-      viewBox={viewBox}
-      width={width}
-      height={height + legendHeight}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-    >
-      {children}
-    </svg>
-  );
+	return (
+		<svg
+			ref={rootRef}
+			viewBox={viewBox}
+			width={width}
+			height={height + legendHeight}
+			onMouseMove={handleMouseMove}
+			onMouseLeave={handleMouseLeave}
+		>
+      <title>{chartID}</title>
+			{children}
+		</svg>
+	);
 };
 
 export default Svg;
